c509375eb111219aaba29e61187a0b5d9df6a285
omensuper1 <136446205+omensuper1@users.noreply.github.com>
2023-12-13 19:14:14 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ const Auth = () => {
     <div className="background">
       <div className="auth-container">
         <form className="auth-form" onSubmit={(e) => onSubmit(e)}>
-          <div className="auth-title">NextJS Chat</div>
+          <div className="auth-title">Dũng NextJS Chat</div>
 
           <div className="input-container">
             <input
